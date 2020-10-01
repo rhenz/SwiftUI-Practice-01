1f50d1fda463d25ba50daad3c15469efbc987979
John Lawrence Salvador <salvador.rhenz@gmail.com>
2020-10-01 22:58:34 +0800

    Added Gradient color to menu view with the help of Color Literal. Able to use Overlay to put an Avatar on top of the Menu View.

--- SwiftUI-Tuts/MenuView.swift
+++ SwiftUI-Tuts/MenuView.swift
@@ -13,16 +13,36 @@ struct MenuView: View {
       VStack {
          Spacer()
          VStack(spacing: 16) {
+            Text("John Salvador - 30% completed")
+               .font(.caption)
+            Color.white
+               .frame(width: 38, height: 6)
+               .cornerRadius(3)
+               .frame(width: 130, height: 6, alignment: .leading)
+               .background(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)).opacity(0.08))
+               .cornerRadius(/*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/)
+               .padding()
+               .frame(width: 150, height: 24)
+               .background(Color.black.opacity(0.1))
+               .cornerRadius(12)
             MenuRow(title: "Account", icon: "gear")
             MenuRow(title: "Billing", icon: "creditcard")
             MenuRow(title: "Sign Out", icon: "person.crop.circle")
          }
          .frame(maxWidth: .infinity)
          .frame(height: 300)
-         .background(Color.white)
+         .background(LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), Color(#colorLiteral(red: 0.8705882353, green: 0.8941176471, blue: 0.9450980392, alpha: 1))]), startPoint: .top, endPoint: .bottom))
          .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
-         .shadow(radius: 30)
+         .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20  )
          .padding(.horizontal, 30)
+         .overlay(
+            Image("Avatar")
+               .resizable()
+               .aspectRatio(contentMode: .fill)
+               .frame(width: 60, height: 60)
+               .clipShape(Circle())
+               .offset(y: -150)
+         )
       }
       .padding(.bottom, 30)
    }
@@ -44,6 +64,7 @@ struct MenuRow: View {
             .font(.system(size: 20, weight: .light))
             .imageScale(.large)
             .frame(width: 32, height: 32)
+            .foregroundColor(Color(#colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)))
          Text(title)
             .font(.system(size: 20, weight: .bold, design: .rounded))
             .frame(width: 120, alignment: .leading)
