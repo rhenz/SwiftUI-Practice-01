6f71a75e7a11e8baa22948f61d465400a71802bf
John Lawrence Salvador <salvador.rhenz@gmail.com>
2020-10-04 23:12:37 +0800

    Learned how to use `@Binding property wrapper` to share value between different components within a file.

--- SwiftUI-Tuts/HomeView.swift
+++ SwiftUI-Tuts/HomeView.swift
@@ -2,78 +2,33 @@
 //  HomeView.swift
 //  SwiftUI-Tuts
 //
-//  Created by Lawrence on 10/2/20.
+//  Created by Lawrence on 10/4/20.
 //  Copyright © 2020 JLCS. All rights reserved.
 //
 
 import SwiftUI
 
 struct HomeView: View {
-   @State var showProfile = false
-   @State var viewState = CGSize.zero
+   @Binding var showProfile: Bool
    
    var body: some View {
-      ZStack {
-         Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1))
-            .edgesIgnoringSafeArea(.all)
-         
-         VStack {
-            HStack {
-               Text("Watching")
-                  .font(.system(size: 28, weight: .bold))
-               
-               Spacer()
-               Button(action: { self.showProfile.toggle() }, label: {
-                  Image("Avatar").renderingMode(.original)
-                     .resizable()
-                     .frame(width: 36, height: 36)
-                     .clipShape(Circle())
-               })
-            }
-            .padding(.horizontal)
-            .padding(.top, 30)
+      VStack {
+         HStack {
+            Text("Watching")
+               .font(.system(size: 28, weight: .bold))
+            
             Spacer()
+            AvatarView(showProfile: $showProfile)
          }
-         .padding(.top, 44)
-         .background(Color.white)
-         .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
-         .shadow(color: Color.blue.opacity(0.2), radius: 20, x: 0, y: 20)
-         .offset(y: showProfile ? -450 : 0)
-         .rotation3DEffect(Angle(degrees: showProfile ? Double(viewState.height / 10) - 10 : 0), axis: (x: 10, y: 0, z: 0))
-         .scaleEffect(showProfile ? 0.9 : 1)
-         .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
-         .edgesIgnoringSafeArea(.all)
-         
-         
-         MenuView()
-            .background(Color.black.opacity(0.001)) // Hack solution in order to make the Menu View interactive
-            .offset(y: showProfile ? 0 : UIScreen.main.bounds.height)
-            .offset(y: viewState.height)
-            .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
-            .onTapGesture {
-               self.showProfile.toggle()
-            }
-            .gesture(
-               DragGesture().onChanged { value in
-                  if value.translation.height >= 0 {
-                     self.viewState = value.translation
-                  }
-               }
-               .onEnded { value in
-                  if self.viewState.height > 100 {
-                     self.showProfile = false
-                  }
-                  self.viewState = .zero
-               }
-            )
-         
-         Text(self.viewState.debugDescription)
+         .padding(.horizontal)
+         .padding(.top, 30)
+         Spacer()
       }
    }
 }
 
 struct HomeView_Previews: PreviewProvider {
    static var previews: some View {
-      HomeView()
+      HomeView(showProfile: .constant(false))
    }
 }
